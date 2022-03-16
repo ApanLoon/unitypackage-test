dd640db7f4db0156b14d838f25281eea9bb1c033
ApanLoon <apanloon@live.co.uk>
2022-03-16 09:24:42 +0100

    Project with package folder inside.

---
+++ src/unitypackage-test/Packages/com.apanloon.test-package/Runtime/TestPackageBehaviour.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace com.apanloon.unitypackagetest
+{
+    public class TestPackageBehaviour : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("TestPackageBehaviour.Start");
+        }
+    }
+}
